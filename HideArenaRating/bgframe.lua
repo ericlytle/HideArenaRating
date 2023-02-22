2bb915de5abdee4037299d075bfb897784ad207d
Eric Lytle <elytle2009@gmail.com>
2023-02-22 14:12:53 -0600

    removing third column for solo shuffle.

--- HideArenaRating/bgframe.lua
+++ HideArenaRating/bgframe.lua
@@ -2,6 +2,7 @@ function removeColumns(tableBuilder, dataIndex)
 	if C_PvP.IsRatedBattleground() or C_PvP.IsRatedArena() or C_PvP.IsRatedSoloShuffle() and not IsArenaSkirmish() then
 		tremove(tableBuilder.columns);
 		tremove(tableBuilder.columns);
+		tremove(tableBuilder.columns);
 	end
 end
 
